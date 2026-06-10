c9abf609049e10384ce5a11f3fc717f21d0fc7d5
nurultazqyah <240306026.mhs@uinmataram.ac.id>
2026-06-10 22:48:47 +0800

    Update Transaksi_bengkel.php

--- Transaksi_bengkel.php
+++ Transaksi_bengkel.php
@@ -4,3 +4,20 @@ include "koneksi.php";
 if (!isset($_SESSION['petugas'])) {
     header("Location: login.php");
 }
+
+if (isset($_POST['simpan'])) {
+    $no_plat       = $_POST['no_plat'];
+    $layanan       = $_POST['layanan'];
+    $biaya_layanan = $_POST['biaya_layanan'];
+    $id_barang     = $_POST['id_barang'];
+    $jumlah        = $_POST['jumlah'];
+    $total_harga   = $_POST['total_harga'];
+
+    mysqli_query($koneksi, "INSERT INTO transaksi VALUES ('','$no_plat','$layanan','$biaya_layanan','$id_barang','$jumlah','$total_harga',NOW())");
+    
+    if (!empty($id_barang) && $jumlah>0) {
+        mysqli_query($koneksi, "UPDATE barang SET stok=stok-$jumlah WHERE id_barang='$id_barang'");
+    }
+    echo "<script>location='nota.php?id=".mysqli_insert_id($koneksi)."';</script>";
+}
+
